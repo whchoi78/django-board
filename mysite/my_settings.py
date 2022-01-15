12032a45e568a4d4e32394c9a03288e74779cea0
whchoi78 <caliper78@naver.com>
2022-01-16 01:44:04 +0900

    1.0

---
+++ mysite/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+    'ENGINE'    : 'django.db.backends.mysql', 
+    'NAME'      : 'django', 
+    'USER'      : 'sre4s',
+    'PASSWORD'  :'',
+    'HOST'      : '',
+    'PORT'      : '3306',
+    }
+}
